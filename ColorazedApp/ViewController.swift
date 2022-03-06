235973836dd3f1b392d4df90476234c2fc755686
Dymitr12 <dk39fra@gmail.com>
2022-03-06 10:56:25 +0300

    Colorzed

--- ColorazedApp/ViewController.swift
+++ ColorazedApp/ViewController.swift
@@ -9,11 +9,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var colorView: UIView!
+    
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+   
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+     
+        redSlider.value = 0.1
+        greenSlider.value = 0.1
+        blueSlider.value = 0.1
+       
+        redLabel.text = String(redSlider.value)
+        greenLabel.text = String(greenSlider.value)
+        blueLabel.text = String(blueSlider.value)
+        
+        colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+       
+        
     }
-
-
+    override func viewDidLayoutSubviews() {
+ 
+        colorView.layer.cornerRadius = colorView.frame.width / 60
+  }
+    
+    @IBAction func redSliderAction() {
+        redLabel.text = String(redSlider.value)
+    }
+    @IBAction func greenSliderAction() {
+        greenLabel.text = String(greenSlider.value)
+    }
+    @IBAction func blueSliderAction() {
+        blueLabel.text = String(blueSlider.value)
+    }
 }
 
